a6ce4cd0c23f22d1acd1304eb396d142ab44ffbb
Prashannakumar <a.prashanna1996@gmail.com>
2026-01-04 23:44:24 +0530

    basic CRUD for employee entity done

---
+++ src/main/java/com/example/crud/entity/Employee.java
@@ -0,0 +1,52 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+
+    private String employeeName;
+
+    private Float employeeSalary;
+
+    public Employee(){
+    }
+
+    public Employee(Long employeeId, String employeeName, Float employeeSalary){
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName(){
+        return this.employeeName;
+    }
+
+    public float getEmployeeSalary(){
+        return this.employeeSalary;
+    }
+
+    public void setEmployeeId(Long employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public void setEmployeeName(String employeeName){
+        this.employeeName = employeeName;
+    }
+
+    public void setEmployeeSalary(float employeeSalary){
+        this.employeeSalary = employeeSalary;
+    }
+
+}
